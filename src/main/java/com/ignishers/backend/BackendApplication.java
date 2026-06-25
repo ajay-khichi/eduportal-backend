40ec9d67c1580ed29d340b898a41638f9f1ab58e
ajay-khichi <ajaysinghkhichi5@gmail.com>
2026-06-25 15:41:50 +0530

    Sets application's default timezone to UTC.

--- src/main/java/com/ignishers/backend/BackendApplication.java
+++ src/main/java/com/ignishers/backend/BackendApplication.java
@@ -3,10 +3,17 @@ package com.ignishers.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class BackendApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
